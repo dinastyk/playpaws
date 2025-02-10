e7768ebf45737ff143f4810bfa6c276b9ae15dd3
mattc7660 <mdcking4749@live.com>
2025-02-10 12:48:10 -0500

    basic file

---
+++ testingFlutter/testingFlutter/testingFlutterApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingFlutterApp.swift
+//  testingFlutter
+//
+//  Created by Matthew Charles on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct testingFlutterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testingFlutter/testingFlutterTests/testingFlutterTests.swift
@@ -0,0 +1,17 @@
+//
+//  testingFlutterTests.swift
+//  testingFlutterTests
+//
+//  Created by Matthew Charles on 2/10/25.
+//
+
+import Testing
+@testable import testingFlutter
+
+struct testingFlutterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
