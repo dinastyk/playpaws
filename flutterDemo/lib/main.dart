f4cf543148469456aefc511f6b279f6be8e5b18c
mattc7660 <mdcking4749@live.com>
2025-02-14 20:41:57 -0500

    removed base code

--- flutterDemo/lib/main.dart
+++ flutterDemo/lib/main.dart
@@ -77,7 +77,7 @@ class _MyHomePageState extends State<MyHomePage>
     Center(child: Text("More Page")),
   ];
 
-  void _TapPressed(int index) 
+  void _tapPressed(int index) 
   {
     setState(() 
     {
@@ -115,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage>
       bottomNavigationBar: BottomNavigationBar
       (
         currentIndex: _index,
-        onTap: _TapPressed,
+        onTap: _tapPressed,
         items:
         [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
@@ -124,71 +124,6 @@ class _MyHomePageState extends State<MyHomePage>
           //BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: "More"),
         ],
       )
-      /*
-      body: Align
-      (
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        alignment: Alignment.bottomCenter,
-        child: Padding
-        (
-          padding: const EdgeInsets.only(bottom: 20),
-          child: Column
-          (
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            //
-            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-            // action in the IDE, or press "p" in the console), to see the
-            // wireframe for each widget.
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>
-            [
-              /*
-              const Text
-              (
-                'You have pushed the button this many times:',
-              ),
-              Text
-              (
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              */
-              Row
-              (
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: 
-                [
-                  ElevatedButton(onPressed: () {print('Opened Tab 1');},
-                  child: Text('Tab 1'),),
-                  ElevatedButton(onPressed: () {print('Opened Tab 2');},
-                  child: Text('Tab 2'),),
-                  ElevatedButton(onPressed: () {print('Opened Tab 3');},
-                  child: Text('Tab 3'),),
-                  ElevatedButton(onPressed: () {print('Opened Tab 4');},
-                  child: Text('Tab 4'),),
-                ],
-              ),
-            ],
-          ),
-        ),
-        */
       );
-      /*
-      floatingActionButton: FloatingActionButton
-      (
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      */ // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
