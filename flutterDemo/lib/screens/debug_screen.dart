166faab2c1ae47ecfb0c25c5b23da89d5d382547
mattc7660 <mdcking4749@live.com>
2025-03-17 12:00:49 -0400

    comment out debug that's not working + include flutter version in pubspec

--- flutterDemo/lib/screens/debug_screen.dart
+++ flutterDemo/lib/screens/debug_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class DebugScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -6,13 +8,14 @@ class DebugScreen extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
+            /*
             final testDataService = TestDataService();
             testDataService.createUsers();
             testDataService.createDogs();
             testDataService.createPlaydates();
             testDataService.createMatches();
             ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Test data created successfully!')));
-          },
+          */},
           child: Text('Generate Test Data'),
         ),
       ),
